8b71a6bb1b86a2901912caa8b6fd009b300d27ff
Oludamola <oludamolaijimade@gmail.com>
2024-09-20 20:10:11 +0100

    alignment fixed

--- src/app/components/topbar.tsx
+++ src/app/components/topbar.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import React from 'react';
-import { IoSearch, IoNotifications, IoChevronDown } from 'react-icons/io5';
+import { IoSearch, IoChevronDown } from 'react-icons/io5';
 import { MdOutlineKeyboardArrowLeft } from 'react-icons/md';
 
 const Topbar = () => {
@@ -11,17 +11,18 @@ const Topbar = () => {
         <MdOutlineKeyboardArrowLeft className="text-xl mr-2 text-[#667185]" />
         <p className="text-base font-medium text-[#667185]">Back</p>
       </div>
-
-      <div className="relative w-1/2">
+      
+      <div className='flex gap-10'>
+      <div className="relative w-full">
         <input
           type="text"
           placeholder="Search here..."
-          className="w-full p-1 pl-10 border rounded-md focus:outline-none"
+          className="w-[450px] p-1 pl-10 border rounded-md focus:outline-none"
         />
         <IoSearch className="absolute top-1/2 left-3 transform -translate-y-1/2 text-[#667185]" />
       </div>
 
-      <div className="flex items-center gap-6">
+      <div className="flex items-center gap-4">
       <Image src="/assets/notification.svg" alt="logo" width={19} height={23} className='text-[#667185]' />
         <Image src="/assets/comTop.svg" alt="logo" width={23} height={22} className='text-[#667185]' />
         <div className="flex items-center gap-2">
@@ -32,6 +33,7 @@ const Topbar = () => {
           />
           <IoChevronDown className="text-xl text-[#667185]" />
         </div>
+      </div>
       </div>
     </div>
   );
